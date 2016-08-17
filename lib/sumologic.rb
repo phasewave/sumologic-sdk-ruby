4e5ae1640ff69217fcaf044e0ce7bb3960bcbfdb
Dan Rosenbloom <drosenbloom@logicworks.net>
2016-08-17 11:55:45 -0400

    refactor to accomodate redirects to specific endpoints

--- lib/sumologic.rb
+++ lib/sumologic.rb
@@ -1,28 +1,27 @@
-require 'tempfile'
 require 'faraday'
 require 'faraday_middleware'
 require 'faraday-cookie_jar'
 require 'multi_json'
 
 module SumoLogic
-  VERSION = '0.0.5'
-  URL = 'https://api.sumologic.com/api/v1'
+  VERSION = '0.0.5'.freeze
+  HOST = 'api.sumologic.com'.freeze
+  APIVERSION = 'v1'.freeze
+  PATH = 'api'.freeze
 
+  # Sumologic API Client
+  #
+  # Supports search job API, dashboards API, and collectors API.
+  #
   class Client
+    attr_reader :endpoint
 
-    def initialize(access_id=nil, access_key=nil, endpoint=SumoLogic::URL)
-      @endpoint = endpoint
-      @session  = Faraday
-      headers   = {'Content-Type' => 'application/json', 'Accept' => 'application/json'}
-      @session  = Faraday.new(url: @endpoint, headers: headers) do |conn|
-        conn.basic_auth(access_id, access_key)
-        conn.use      FaradayMiddleware::FollowRedirects, limit: 5
-        conn.use      :cookie_jar
-        conn.request  :multipart
-        conn.request  :json
-        conn.response :json, content_type: 'application/json'
-        conn.adapter  Faraday.default_adapter
-      end
+    def initialize(access_id=nil, access_key=nil, endpoint=nil, path=SumoLogic::PATH, version=SumoLogic::APIVERSION, host=SumoLogic::HOST)
+      @endpoint = endpoint ? URI.parse(endpoint) : URI::HTTPS.build(host: host, path: "/#{path}/#{version}")
+      @access_id = access_id
+      @access_key = access_key
+
+      reload_session
     end
 
     def search(query, from_time=nil, to_time=nil, time_zone='UTC')
@@ -85,18 +84,16 @@ module SumoLogic
       return r.body.has_key?('sources') ? r.body['sources'] : nil
     end
 
-    def create_collector(name, type='HTTP', message_per_request=false)
-      Tempfile.open('sumo_create_collector') do |file|
-        file.write({source: {name: name, sourceType: type, messagePerRequest: message_per_request}}.to_json)
-        file.close
-        payload = Faraday::UploadIO.new(file.path, 'application/json')
-        binding.pry
-        r = @session.post do |req|
-          req.url 'collectors'
-          req.body = payload
-        end
-        binding.pry
+    def collector_source(collector_id, source_id)
+      r = @session.get do |req|
+        req.url 'collectors/' + collector_id.to_s + '/sources/' + source_id.to_s
       end
+      return r.body.has_key?('source') ? r.body['source'] : nil
+    end
+
+    def create_source(collector_id, params)
+      url = 'collectors/' + collector_id.to_s + '/sources'
+      post(url, MultiJson.encode(params), 'source')
     end
 
     def dashboard(dashboard_id)
@@ -113,5 +110,45 @@ module SumoLogic
       return r.body.has_key?('dashboardMonitorDatas') ? r.body['dashboardMonitorDatas'] : nil
     end
 
+    private
+
+    def get(url, body, desired_output_key)
+      r = @session.get do |req|
+        req.url url
+        req.body = body
+      end
+
+      return r.body.fetch(desired_output_key, nil) if desired_output_key
+      r
+    end
+
+    def post(url, body, desired_output_key=nil)
+      r = nil
+      loop do
+        r = @session.post do |req|
+          req.url url
+          req.body = body
+        end
+        break if r.to_hash[:url].host == endpoint.host
+        endpoint.host = r.to_hash[:url].host
+        reload_session
+      end
+
+      return r.body.fetch(desired_output_key, nil) if desired_output_key
+      r
+    end
+
+    def reload_session
+      headers   = {'Content-Type' => 'application/json', 'Accept' => 'application/json'}
+      @session  = Faraday.new(url: endpoint.to_s, headers: headers) do |conn|
+        conn.basic_auth(@access_id, @access_key)
+        conn.use      FaradayMiddleware::FollowRedirects, limit: 5
+        conn.use      :cookie_jar
+        conn.request  :multipart
+        conn.request  :json
+        conn.response :json, content_type: 'application/json'
+        conn.adapter  Faraday.default_adapter
+      end
+    end
   end
 end
