ab1fd9e6bf000c42f20d8fcf249783d87a223280
Dan Rosenbloom <drosenbloom@logicworks.net>
2016-08-16 20:02:47 -0400

    adding collectors API, WIP

--- lib/sumologic.rb
+++ lib/sumologic.rb
@@ -1,3 +1,4 @@
+require 'tempfile'
 require 'faraday'
 require 'faraday_middleware'
 require 'faraday-cookie_jar'
@@ -15,7 +16,9 @@ module SumoLogic
       headers   = {'Content-Type' => 'application/json', 'Accept' => 'application/json'}
       @session  = Faraday.new(url: @endpoint, headers: headers) do |conn|
         conn.basic_auth(access_id, access_key)
+        conn.use      FaradayMiddleware::FollowRedirects, limit: 5
         conn.use      :cookie_jar
+        conn.request  :multipart
         conn.request  :json
         conn.response :json, content_type: 'application/json'
         conn.adapter  Faraday.default_adapter
@@ -27,7 +30,7 @@ module SumoLogic
       r = @session.get do |req|
         req.url 'logs/search'
         req.params = params
-      end    
+      end
     end
 
     def search_job(query, from_time=nil, to_time=nil, time_zone='UTC')
@@ -43,7 +46,7 @@ module SumoLogic
         req.url 'search/jobs/' + search_job['id'].to_s
       end
     end
-    
+
     def search_job_records(search_job, limit=nil, offset=0)
       params = {limit: limit, offset: offset}
       r = @session.get do |req|
@@ -59,6 +62,41 @@ module SumoLogic
         req.params = params
       end
       return r.body.has_key?('dashboards') ? r.body['dashboards'] : nil
+    end
+
+    def collectors
+      r = @session.get do |req|
+        req.url 'collectors'
+      end
+      return r.body.has_key?('collectors') ? r.body['collectors'] : nil
+    end
+
+    def collector(collector_id)
+      r = @session.get do |req|
+        req.url 'collectors/' + collector_id.to_s
+      end
+      return r.body.has_key?('collector') ? r.body['collector'] : nil
+    end
+
+    def collector_sources(collector_id)
+      r = @session.get do |req|
+        req.url 'collectors/' + collector_id.to_s + '/sources'
+      end
+      return r.body.has_key?('sources') ? r.body['sources'] : nil
+    end
+
+    def create_collector(name, type='HTTP', message_per_request=false)
+      Tempfile.open('sumo_create_collector') do |file|
+        file.write({source: {name: name, sourceType: type, messagePerRequest: message_per_request}}.to_json)
+        file.close
+        payload = Faraday::UploadIO.new(file.path, 'application/json')
+        binding.pry
+        r = @session.post do |req|
+          req.url 'collectors'
+          req.body = payload
+        end
+        binding.pry
+      end
     end
 
     def dashboard(dashboard_id)
