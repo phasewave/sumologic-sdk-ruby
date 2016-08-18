1c234b18ccf7e15d735969e7c0683eec85545c6b
Dan Rosenbloom <drosenbloom@logicworks.net>
2016-08-18 09:54:49 -0400

    rubocop + return tweaks to get/post

--- lib/sumologic.rb
+++ lib/sumologic.rb
@@ -16,7 +16,7 @@ module SumoLogic
   class Client
     attr_reader :endpoint
 
-    def initialize(access_id=nil, access_key=nil, endpoint=nil, path=SumoLogic::PATH, version=SumoLogic::APIVERSION, host=SumoLogic::HOST)
+    def initialize(access_id = nil, access_key = nil, endpoint = nil, path = SumoLogic::PATH, version = SumoLogic::APIVERSION, host = SumoLogic::HOST)
       @endpoint = endpoint ? URI.parse(endpoint) : URI::HTTPS.build(host: host, path: "/#{path}/#{version}")
       @access_id = access_id
       @access_key = access_key
@@ -24,43 +24,35 @@ module SumoLogic
       reload_session
     end
 
-    def search(query, from_time=nil, to_time=nil, time_zone='UTC')
-      params = {q: query, from: from_time, to: to_time, tz: time_zone}
+    def search(query, from_time = nil, to_time = nil, time_zone = 'UTC')
+      params = { q: query, from: from_time, to: to_time, tz: time_zone }
       r = @session.get do |req|
         req.url 'logs/search'
         req.params = params
       end
     end
 
-    def search_job(query, from_time=nil, to_time=nil, time_zone='UTC')
-      params = {query: query, from: from_time, to: to_time, timeZone: time_zone}
-      r = @session.post do |req|
-        req.url 'search/jobs'
-        req.body = MultiJson.encode(params)
-      end
+    def search_job(query, from_time = nil, to_time = nil, time_zone = 'UTC')
+      params = { query: query, from: from_time, to: to_time, timeZone: time_zone }
+      url = 'search/jobs'
+      post(url, params)
     end
 
-    def search_job_status(search_job={})
-      r = @session.get do |req|
-        req.url 'search/jobs/' + search_job['id'].to_s
-      end
+    def search_job_status(search_job = {})
+      url = 'search/jobs/' + search_job['id'].to_s
+      get(url)
     end
 
-    def search_job_records(search_job, limit=nil, offset=0)
-      params = {limit: limit, offset: offset}
-      r = @session.get do |req|
-        req.url 'search/jobs/' + search_job['id'].to_s + '/records'
-        req.params = params
-      end
+    def search_job_records(search_job, limit = nil, offset = 0)
+      params = { limit: limit, offset: offset }
+      url = 'search/jobs/' + search_job['id'].to_s + '/records'
+      get(url, params, 'records')
     end
 
-    def dashboards(monitors=false)
-      params = {dashboards: monitors}
-      r = @session.get do |req|
-        req.url 'dashboards'
-        req.params = params
-      end
-      return r.body.has_key?('dashboards') ? r.body['dashboards'] : nil
+    def dashboards(monitors = false)
+      params = { dashboards: monitors }
+      url = 'dashboards'
+      get(url, params, url)
     end
 
     def collectors
@@ -69,17 +61,17 @@ module SumoLogic
 
     def collector(collector_id)
       url = 'collectors/' + collector_id.to_s
-      get(url, 'collector')
+      get(url, key: 'collector')
     end
 
     def collector_sources(collector_id)
       url = 'collectors/' + collector_id.to_s + '/sources'
-      get(url, 'sources')
+      get(url, key: 'sources')
     end
 
     def collector_source(collector_id, source_id)
       url = 'collectors/' + collector_id.to_s + '/sources/' + source_id.to_s
-      get(url, 'source')
+      get(url, key: 'source')
     end
 
     def create_source(collector_id, params)
@@ -88,38 +80,34 @@ module SumoLogic
     end
 
     def dashboard(dashboard_id)
-      r = @session.get do |req|
-        req.url 'dashboards/' + dashboard_id.to_s
-      end
-      return r.body.has_key?('dashboard') ? r.body['dashboard'] : nil
+      url = 'dashboards/' + dashboard_id.to_s
+      get(url, key: 'dashboard')
     end
 
     def dashboard_data(dashboard_id)
-      r = @session.get do |req|
-        req.url 'dashboards/' + dashboard_id.to_s + '/data'
-      end
-      return r.body.has_key?('dashboardMonitorDatas') ? r.body['dashboardMonitorDatas'] : nil
+      url = 'dashboards/' + dashboard_id.to_s + '/data'
+      get(url, key: 'dashboardMonitorDatas')
     end
 
     private
 
-    def get(url, desired_output_key=nil)
-      desired_output_key ||= url
+    def get(url, body = nil, key = nil)
       r = nil
       loop do
         r = @session.get do |req|
           req.url url
+          req.body = body
         end
         break if r.to_hash[:url].host == endpoint.host
         endpoint.host = r.to_hash[:url].host
         reload_session
       end
 
-      return r.body.fetch(desired_output_key, nil) if desired_output_key
-      r
+      return r.body.fetch(key, nil) if key
+      r.body
     end
 
-    def post(url, body, desired_output_key=nil)
+    def post(url, body, key = nil)
       r = nil
       loop do
         r = @session.post do |req|
@@ -131,13 +119,12 @@ module SumoLogic
         reload_session
       end
 
-      return r.body.fetch(desired_output_key, nil) if desired_output_key
-      r
+      return r.body.fetch(key, nil) if key
+      r.body
     end
 
     def reload_session
-      headers   = {'Content-Type' => 'application/json', 'Accept' => 'application/json'}
-      @session  = Faraday.new(url: endpoint.to_s, headers: headers) do |conn|
+      @session  = Faraday.new(url: endpoint.to_s) do |conn|
         conn.basic_auth(@access_id, @access_key)
         conn.use      FaradayMiddleware::FollowRedirects, limit: 5
         conn.use      :cookie_jar
